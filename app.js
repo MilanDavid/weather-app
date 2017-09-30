f7e8cef76088a7f8543823fb3c3399cedbfe5509
MilanDavid <miland.sm@gmail.com>
2017-09-30 17:36:10 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+"use strict";
+
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+    loadingText.style.display = 'block';
+    weatherBox.style.display = 'none';
+    var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a City Name');
+    }
+    var http = new XMLHttpRequest();
+    var apiKey = 'a3716dc572738c22c58d920afd735556';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&APPID=' + apiKey;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+			setTimeout(function(){updateWeather(weatherData)},2000);
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert('Something went wrong!');
+        }
+    };
+    http.send();
+}
+
+function updateWeather(weatherData) {
+    weatherCity.textContent = weatherData.cityName;
+    weatherDescription.textContent = weatherData.description;
+    weatherTemperature.textContent = weatherData.temperature;
+
+    loadingText.style.display = 'none';
+    weatherBox.style.display = 'block';
+}
